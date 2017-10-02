556232ec6221a30b9f52277d166a279a97044f59
Jirawat Thaenthong <jirawat.t@phuket.psu.ac.th>
2017-10-02 21:32:15 +0700

    add broadcasting event

--- server/server.js
+++ server/server.js
@@ -15,14 +15,30 @@ io.on('connection', (socket) => {
   console.log('New user connected');
 
   socket.emit('newMessage', {
-    from: 'Somsri',
-    text: 'See you then',
-    createdAt: 123123
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
   });
 
-  socket.on('createMessage', (message) => {
-    console.log('createMessage', message);
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
   });
+
+  // socket.on('createMessage', (message) => {
+  //   console.log('createMessage', message);
+  //   io.emit('newMessage', {
+  //     from: message.from,
+  //     text: message.text,
+  //     createdAt: new Date().getTime()
+  //   });
+  //   // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+  // });
 
   socket.on('disconnect', () => {
     console.log('User was disconnected');
