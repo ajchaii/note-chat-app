f3a9846403e98c768004e4eaabb055ef4396e634
Jirawat Thaenthong <jirawat.t@phuket.psu.ac.th>
2017-10-03 23:54:24 +0700

    add generate message function

---
+++ server/utils/message.js
@@ -0,0 +1,15 @@
+var generateMessage = (from, text) => {
+  var date = new Date().getDate();
+  var month = new Date().getMonth();
+  var year = new Date().getFullYear();
+  var hour = new Date().getHours();
+  var min = new Date().getMinutes();
+  var sec = new Date().getSeconds();
+  return {
+    from,
+    text,
+    createdAt: `${date}/${month}/${year}, ${hour}:${min}:${sec}`
+  };
+};
+
+module.exports = {generateMessage};
