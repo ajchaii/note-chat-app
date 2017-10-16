26d201df54bcfedc0bd1d691e620b20a12888eba
Jirawat Thaenthong <jirawat.t@phuket.psu.ac.th>
2017-10-16 17:58:38 +0700

    css sytles for chat app

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,14 +1,8 @@
 var generateMessage = (from, text) => {
-  var date = new Date().getDate();
-  var month = new Date().getMonth();
-  var year = new Date().getFullYear();
-  var hour = new Date().getHours();
-  var min = new Date().getMinutes();
-  var sec = new Date().getSeconds();
   return {
-      from,
-      text,
-      createdAt: `${date}/${month}/${year}, ${hour}:${min}:${sec}`
+    from,
+    text,
+    createdAt: new Date().getTime()
   };
 };
 
