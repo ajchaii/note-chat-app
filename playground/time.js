243d8e1f2a4896dc2a744fb86ffedaa182b133a6
Jirawat Thaenthong <jirawat.t@phuket.psu.ac.th>
2017-11-01 23:57:58 +0700

    add joining page

--- playground/time.js
+++ playground/time.js
@@ -16,6 +16,6 @@ var moment = require('moment');
 var someTimestamp = moment().valueOf();
 console.log(someTimestamp)
 
-//var createdAt = 1234;
-var date = moment(someTimestamp);
+var createdAt = 1234;
+var date = moment(createdAt);
 console.log(date.format('h:mm a'))
